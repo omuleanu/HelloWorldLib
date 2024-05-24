3878ff7f1ec9ceaa3cb9eb1d177c38753b8cf3b0
Val <vnexster@gmail.com>
2024-05-24 17:59:59 +0300

    added xml docs, nuget package ico

--- HelloWorldLib/HelloWorld.cs
+++ HelloWorldLib/HelloWorld.cs
@@ -1,7 +1,14 @@
 ﻿namespace HelloWorldLib
 {
+    /// <summary>
+    /// Hello World util
+    /// </summary>
     public static class HelloWorld
     {
+        /// <summary>
+        /// will return "Hello World"
+        /// </summary>
+        /// <returns></returns>
         public static string Say()
         {
             return "Hello World";
